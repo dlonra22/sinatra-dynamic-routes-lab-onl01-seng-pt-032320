34e128fcac2497d9032ab13bbb7248d3d93b8bad
arnie b <arnold.bwaila@gmail.com>
2020-07-07 12:54:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -24,14 +24,15 @@ class App < Sinatra::Base
   end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @word1 = params[:word1] + " "
-    @word2 = params[:word2] + " "
-    @word3 = params[:word3] + " "
-    @word4 = params[:word4] + " "
-    @word5 = params[:word5]
+    @word = []
+    @word << params[:word1] + " "
+    @word << params[:word2] + " "
+    @word << params[:word3] + " "
+    @word << params[:word4] + " "
+    @word << params[:word5]
     @longword = ""
-    5.times do |i|
-      @longword = @longword + @word(i)
+    @word.each do |w|
+      @longword = @longword + w
     end
     @longword
   end
